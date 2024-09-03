b9516045e837ed3ff5c13a9219f353e78b80fffd
vaghelaharshad <vaghelaharshad@5648gmail.com>
2024-09-03 13:39:32 +0530

    c progrmming

---
+++ 05 day/03fgvjv.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+
+
+main() {
+    int num1, num2, num3;
+ int min;
+   
+    printf("Enter the value of the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the value of the second number: ");
+    scanf("%d", &num2);
+    printf("Enter the value of the third number: ");
+    scanf("%d", &num3);
+
+   
+    if (num1 <= num2) {
+        if (num1 <= num3) {
+            min = num1;  
+        } else {
+            min = num3;  
+        }
+    } else {
+        if (num2 <= num3) {
+            min = num2;  
+        } else {
+            min = num3; 
+        }
+    }
+
+  
+    printf("The miximum value is: %d\n", min);
+
+   
+
+	
+}
+
