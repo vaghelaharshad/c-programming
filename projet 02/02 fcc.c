cad3a4f32c587bcbacd1e5bb150e5fb581cdf803
vaghelaharshad <vaghelaharshad@5648gmail.com>
2024-09-04 11:49:32 +0530

    projet 02

---
+++ projet 02/02 fcc.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+
+
+main(){
+	  int score;
+      char grade;
+    
+     printf("Enter your score (out of 100): ");
+      scanf("%d", &score);
+
+    grade = (score <= 90) ? 'A' : (score <= 80) ? 'B' : (score >= 70) ? 'C' :
+            (score >= 60) ? 'D' : (score >= 50) ? 'E' : 'F';
+
+    printf("Your grade is %c. ", grade);
+
+    switch (grade) {
+    	
+        case 'A':
+            printf("Excellent work!");
+            break;
+        case 'B':
+            printf("Well done!");
+            break;
+        case 'C':
+            printf("Good job!");
+            break;
+        case 'D':
+            printf("You passed, but you could do better.");
+            break;
+        case 'E':
+            printf("You passed, but need improvement.");
+            break;
+        case 'F':
+            printf("Sorry, you failed.");
+            break;
+        default:
+            printf("Invalid grade.");
+            break;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
