c506fd1600ae3b44c87fa0a9c1b11613afa3bb60
vaghelaharshad <vaghelaharshad@5648gmail.com>
2024-09-06 10:24:49 +0530

    day02

---
+++ day 02/01jcjkc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+main(){
+	   float a,b,ans;
+	   
+	   printf("enter a;");
+	   scanf("%f",&a);
+	   
+	   printf("enter b;");
+	   scanf("%f",&b);
+	   ans=a+b;
+	   printf("ans is: %f",ans);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+
